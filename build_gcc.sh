94d7edbf3536f64f75e592898db52f3ed889ed06
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-06-24 17:59:18 +0300

    add lzma compression, and hardware cypto

--- build_gcc.sh
+++ build_gcc.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 [ -d "out" ] && rm -rf out || mkdir -p out
-[ -d "los-4.9-64" ] && echo "ARM64 TC Present." || echo "ARM64 TC Not Present. Downloading..." | git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
-[ -d "los-4.9-32" ] && echo "ARM32 TC Present." || echo "ARM32 TC Not Present. Downloading..." | git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
 
 make O=out ARCH=arm64 lima_defconfig
 
-PATH="$(pwd)/los-4.9-64/bin:$(pwd)/los-4.9-32/bin:${PATH}" \
+PATH="/home/akham/proj/google/arm64/bin:/home/akham/proj/gogole/arm32/bin:${PATH}" \
 make                  O=out \
                       ARCH=arm64 \
                       CROSS_COMPILE=aarch64-linux-android- \
